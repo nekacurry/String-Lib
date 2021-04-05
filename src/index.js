8947f59483e1a6c6db6f54e42688d172684f847d
nekacurry <anneka498@gmail.com>
2021-04-05 13:42:56 -0500

    completed challenges 1 and 2

---
+++ src/index.js
@@ -0,0 +1,21 @@
+
+// CHALLENGE 1 - Capitalize first letter of string
+function capitalize(str) {
+  var first = str[0].toUpperCase()
+  var rest = str.slice(1)
+  return first + rest
+}
+
+console.log(capitalize("hello world!"))
+
+// CHALLENGE 2 - Capitalize entire string
+function allCaps(str) {
+  var capStr = str.toUpperCase()
+}
+
+console.log(allCaps("hello world!"))
+
+// CHALLENGE 3 - Capitalize first letter of each word
+function capitalizeWords(str) {
+  
+}
